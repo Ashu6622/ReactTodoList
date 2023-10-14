91eed8bc7f648c921e0be13ff9e3f684eea62232
Ashutosh <rashu2879@gmail.com>
2023-10-14 07:42:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Inputtasks from './components/Inputtasks';
+import { useState} from 'react';
 function App() {
+
+  const[task,settask] = useState([]);
+  const[temp ,settemp] = useState("");
+
+  function writeTask(e){
+    settemp(e.target.value);
+  }
+
+  function addtask(){
+    if(temp!=""){
+    settask([...task,temp]);
+    settemp("");
+    }
+  }
+  function Deletetask(index){
+
+    console.log(index);
+    
+    // const x = temp[index];
+
+    const filterdata = task.filter((e,i)=> {
+      if(index!=i){
+        return e;
+      }
+    })
+    // console.log(filterdata);
+
+    settask(filterdata);
+   
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+      <h1>Todo List</h1>
+
+      <div className="tasklists">
+
+        <Inputtasks writeTask={writeTask} addtask={addtask} temp={temp}/>
+
+        <div className="todotasks">
+
+          {task.map((element,index)=>{
+            return(
+              <div className="list">
+              <p key={index}>{element} </p><button onClick={()=>{Deletetask(index)}} className='deletebtn'>Delete</button>
+              </div>
+            )
+          })}
+
+        </div>
+
+      </div>
+      </div>
+     
     </div>
   );
 }
